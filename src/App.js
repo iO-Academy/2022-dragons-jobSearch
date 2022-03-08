e9073b337b8ce2671d4a5a66df62339dbc5eb02a
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-03-08 09:14:35 +0000

    committing changes to src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './normalize.css'
 import './App.scss';
+import RecentJobs from "./RecentJobs/recentJobs";
+
 
 function App() {
+
+    return (
+        <>
+            <RecentJobs/>
+        </>
+        )
+
+
 
 }
 
 export default App;
+
