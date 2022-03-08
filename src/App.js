55288ced4a858ead1f27a31c164b77d78ebed5dd
Marcus Drury <marcusdrury@outlook.com>
2022-03-08 10:12:12 +0000

    Work in progress, working on skill tags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './normalize.css'
 import './App.scss';
+import SkillTag from "./SkillTag/SkillTag";
 
 function App() {
-
+    const skillTag = 'hello'
+    return(
+        <SkillTag skillTag={skillTag} />
+    )
 }
 
 export default App;
