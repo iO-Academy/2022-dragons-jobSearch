e5fcf86f3a1c42b0cbb8128f34732d32eada3597
Marcus Drury <marcusdrury@outlook.com>
2022-03-10 12:30:53 +0000

    Added a button that will link to either all jobs or recent jobs and will change when clicked

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './normalize.css'
 import './App.scss';
 import {useState} from 'react'
 import SearchJobs from "./SearchJobs/SearchJobs";
+import ToggleJobsView from "./ToggleJobsView/ToggleJobsView";
 
 
 function App() {
     return (
             <>
+                <ToggleJobsView />
                 <SearchJobs />
             </>
         )
